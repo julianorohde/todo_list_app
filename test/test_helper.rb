ad107e01c3cb042db523dbbb234c9c68a1ff9880
Juliano Rohde <julianorohde@hotmail.com>
2024-12-18 14:01:05 -0400

    Configure the rails_helper to write and run tests correctly

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,25 @@
+# frozen_string_literal: true
+
 ENV["RAILS_ENV"] ||= "test"
+
 require_relative "../config/environment"
 require "rails/test_help"
+require 'faker'
+require 'simplecov'
+require 'shoulda/matchers'
+require 'shoulda/context'
 
-module ActiveSupport
-  class TestCase
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+SimpleCov.start
 
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
+# This code loads all Ruby files in the test/support directory and its subdirectories.
+Dir[Rails.root.join("test/support/**/*.rb")].each { |f| require f }
 
-    # Add more helper methods to be used by all tests here...
-  end
+class ActiveSupport::TestCase
+  include FactoryBot::Syntax::Methods
+  
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors)
+
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
 end
