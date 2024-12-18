37019a82392f13ca8bfa896e53960645100e0594
Juliano Rohde <julianorohde@hotmail.com>
2024-12-18 15:15:30 -0400

    Create and configure the initial pages for login, sign-up, and the main page to manage the to-do items

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  authenticated :user do
+    root to: "todos#index", as: :authenticated_root
+  end
+
+  unauthenticated do
+    devise_scope :user do
+      root to: "devise/sessions#new", as: :unauthenticated_root
+    end
+  end
 end
